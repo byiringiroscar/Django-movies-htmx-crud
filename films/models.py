ded4a8ea7df2c37903fccf1c9fdd839c0bee4bfa
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-28 07:33:28 +0200

    wip on add film dynamically

--- films/models.py
+++ films/models.py
@@ -3,3 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Film(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    users = models.ManyToManyField(User, related_name='films') # this related will help us to get all films of a user like user.films.all()
+
+    def __str__(self):
+        return self.name
